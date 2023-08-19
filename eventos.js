fee39754853dd8a42558dedeff971e6fd84c164c
alfredosole <137563812+AlfreSole@users.noreply.github.com>
2023-08-19 19:42:33 -0300

    eventos-3

--- eventos.js
+++ eventos.js
@@ -1,4 +1,11 @@
-const saludarBtn = document.getElementById("saludarBtn");
-function saludar() {
-    alert("¡Hola! ¡Bienvenido!");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const saludarBtn = document.getElementById("saludarBtn");
+    saludarBtn.addEventListener("click", function() {
+        alert("¡Hola! ¡Bienvenido!");
+    });
+
+    const miDiv = document.getElementById("miDiv");
+    miDiv.addEventListener("click", function() {
+        alert("Hola! Soy el div");
+    });
+});
